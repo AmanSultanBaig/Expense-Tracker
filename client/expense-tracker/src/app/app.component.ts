628f6107c72756c8bda7d02af78d123367d06f41
Aman Sultan Baig <amansultanbaig@gmail.com>
2024-09-20 15:51:29 +0500

    header added to application

--- client/expense-tracker/src/app/app.component.ts
+++ client/expense-tracker/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';  // Import PrimeNG modules here
 import { InputTextModule } from 'primeng/inputtext';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonModule, InputTextModule, RouterOutlet],  // Import PrimeNG modules here
+  imports: [ButtonModule, InputTextModule, RouterOutlet, HeaderComponent],  // Import PrimeNG modules here
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ client/expense-tracker/src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [HttpClientModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  providers: [ApiService]
+})
+export class HeaderComponent {
+  userName:string = ""
+  constructor(private _apiService: ApiService){
+    const loggedInUser = this._apiService.getLoggedInUser();
+    this.userName = loggedInUser.name?.split(" ")[0][0];
+    this.userName += loggedInUser.name?.split(" ")[1][0];
+  }
+}
